3f93856668b4c70243a641b26d86e48073a9a585
Christopher Rizzo <christophermatthewrizzo@gmail.com>
2017-07-02 17:16:47 -0400

    feat (implement props.ContainerStyle, remove buttonHolder css)

--- src/animatedButton.jsx
+++ src/animatedButton.jsx
@@ -27,6 +27,17 @@ export default class Button extends Component {
         this.buttonText = props.buttonText || 'hover me!';
         this.animatedText = props.animatedText;
 
+        this.containerStyle = {opacity: 1,
+                               boxSizing: "border-box",
+                               display: "flex",
+                               flexDirection: "column",
+                               justifyContent: "center",
+                               position: "relative",
+                               textAlign: "center",
+                               width: this.width,
+                               height: this.height
+                                ...props.containerStyle};
+
         this.path = props.path || "M10,10 C10,10 50,9.98999977 90,9.98999977 C130,9.98999977 170,10 170,10 C170,10 170.009995,20 170.009995,30 C170.009995,40 170,50 170,50 C170,50 130,50.0099983 90,50.0099983 C50,50.0099983 10,50 10,50 C10,50 9.98999977,40 9.98999977,30 C9.98999977,20 10,10 10,10 Z";
         this.animatedPath = props.animatedPath || "M10,10 C10,10 50,7 90,7 C130,7 170,10 170,10 C170,10 172,20 172,30 C172,40 170,50 170,50 C170,50 130,53 90,53 C50,53 10,50 10,50 C10,50 8,40 8,30 C8,20 10,10 10,10 Z";
     }
@@ -49,7 +60,7 @@ export default class Button extends Component {
             <a className="buttonHolder"
                onMouseEnter={ this.onMouseEnter }
                onMouseLeave={ this.onMouseLeave }
-               style={{"width": this.width, "height": this.height}}
+               style={this.containerStyle}
                 >
                 <svg viewBox={`0 0 180 60`} className="buttonSVG" style={this.pathStyle} {...this.props.additionalButtonProps}>
                     <Anime
@@ -82,7 +93,7 @@ export default class Button extends Component {
             <a className="buttonHolder"
                onMouseEnter={ this.onMouseEnter }
                onMouseLeave={ this.onMouseLeave }
-               style={{"width": this.width, "height": this.height}}
+               style={this.containerStyle}
                 >
                     <svg
                         viewBox={`0 0 180 60`}
