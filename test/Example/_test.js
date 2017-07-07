f69de607173b5ef00d461569c77d04483f424352
Christopher Rizzo <christophermatthewrizzo@gmail.com>
2017-07-07 17:20:32 -0400

    feat (test now correctly checks normal and hover states!)

--- test/Example/_test.js
+++ test/Example/_test.js
@@ -3,26 +3,25 @@ var ReactDOM = require('react-dom');
 var ReactTestUtils = require('react-dom/test-utils');
 var Button = require('../../src/animatedButton.jsx');
 
-// var renderer = require('react-test-renderer');
-
 const { expect } = require('chai');
-const { mount } = require('enzyme');
 
-
-// mount the button
 describe('animeButton', () => {
   it('swaps between normal and hover views', () => {
-    // const wrapper = mount(<Button />);
     const wrapper = ReactTestUtils.renderIntoDocument(<Button />); // <-- is Composite Component
     let container = ReactDOM.findDOMNode(wrapper); // <-- is DOM Component
     expect(container).to.exist;
+
+    // Test Normal State & View
     expect(wrapper.state.inButton).to.be.false;
-    console.log(ReactTestUtils.findRenderedDOMComponentWithClass(wrapper, 'normalText'));
-    // see normal text?
+    expect(ReactTestUtils.findRenderedDOMComponentWithClass(wrapper, 'normalText')).to.exist;
+    expect(ReactTestUtils.scryRenderedDOMComponentsWithClass(wrapper, 'hoverText')).to.have.length(0);
+
+    // Simulate Mouse Enter
     ReactTestUtils.Simulate.mouseEnter(container);
+
+    // Test Hover State & View
     expect(wrapper.state.inButton).to.be.true;
-    // see hover text?
-    let elems = ReactTestUtils.findAllInRenderedTree(wrapper, () => true);
-    // findAllInRenderedTree must be a composite component
+    expect(ReactTestUtils.findRenderedDOMComponentWithClass(wrapper, 'hoverText')).to.exist;
+    expect(ReactTestUtils.scryRenderedDOMComponentsWithClass(wrapper, 'normalText')).to.have.length(0);
   })
 })
