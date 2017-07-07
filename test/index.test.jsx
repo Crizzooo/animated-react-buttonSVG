f3b90fbbf44bd5bd67fe8ccba8728e5f86896606
Christopher Rizzo <christophermatthewrizzo@gmail.com>
2017-07-07 10:13:40 -0400

    bug (mocha runs tests, but cannot simulate on button bc anime library)

--- test/index.test.jsx
+++ test/index.test.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import ReactTestUtils from 'react-dom/test-utils';
 import Button from '../src/animatedButton.jsx';
+
+import renderer from 'react-test-renderer';
+
 const { expect } = require('chai');
 const { mount } = require('enzyme');
 
@@ -8,11 +12,31 @@ const { mount } = require('enzyme');
 // mount the button
 describe('animeButton', () => {
   it('swaps between normal and hover views', () => {
-    const wrapper = mount(<Button />);
-    // const wrapperz = ReactTestUtils.renderIntoDocument(<div></div>);
-    expect(wrapper.find('.normalText')).to.exist;
-    expect(wrapper.find('.hoverText')).to.exist;
+    // const wrapper = mount(<Button />);
+    console.log(ReactTestUtils.isElement(<Button/>))
+    const wrapper = ReactTestUtils.renderIntoDocument(<Button/>);
+    console.log(ReactTestUtils.isDOMComponent(wrapper));
+    let container = ReactDOM.findDOMNode(wrapper);
+
+    // expect(wrapper.find('.normalText')).to.exist;
+    // expect(wrapper.find('.hoverText')).to.exist;
+    //
+    //
+    // let component = wrapper.find('a');
+    // console.log(wrapper.state());
+    // console.log(wrapper.getNode().onMouseEnter());
     // wrapper.simulate('mouseEnter');
+
+    // const element = renderer.create(<Button/>, {
+    //   createNodeMock: element => ({ nodeType: true})
+    // });
+    // console.log(Button.prototype);
+    // console.log(wrapper);
+    ReactTestUtils.Simulate.mouseEnter(container);
+    // console.log(wrapper);
+    // const mock = ReactTestUtils.mockComponent(<Button/>);
+    // console.log(mock);
+    // console.log(element.toJSON());
   })
 })
 // check path or non hover
