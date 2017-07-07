b899a8eff94c49e0c1d27ea9f2bf392850b61c04
Christopher Rizzo <christophermatthewrizzo@gmail.com>
2017-07-07 17:04:47 -0400

    feat (button now sets  this.hoverTextClassName and this.normalTextClassName to their respective props, or default normalText hoverText class names)

--- src/animatedButton.jsx
+++ src/animatedButton.jsx
@@ -43,6 +43,9 @@ class Button extends Component {
 
         this.path = props.path || "M10,10 C10,10 50,9.98999977 90,9.98999977 C130,9.98999977 170,10 170,10 C170,10 170.009995,20 170.009995,30 C170.009995,40 170,50 170,50 C170,50 130,50.0099983 90,50.0099983 C50,50.0099983 10,50 10,50 C10,50 9.98999977,40 9.98999977,30 C9.98999977,20 10,10 10,10 Z";
         this.animatedPath = props.animatedPath || "M10,10 C10,10 50,7 90,7 C130,7 170,10 170,10 C170,10 172,20 172,30 C172,40 170,50 170,50 C170,50 130,53 90,53 C50,53 10,50 10,50 C10,50 8,40 8,30 C8,20 10,10 10,10 Z";
+
+        this.hoverTextClassName = props.hoverTextClassName || 'hoverText';
+        this.normalTextClassName = props.normalTextClassName || 'normalText';
     }
 
     onMouseEnter() {
@@ -85,7 +88,7 @@ class Button extends Component {
                     offset="0"
                     {...this.props.textAnimationProps}
                 >
-                    <div style={textStyle} className={this.props.hoverTextClassName}>{this.animatedText || this.buttonText}</div>
+                    <div style={textStyle} className={this.hoverTextClassName}>{this.animatedText || this.buttonText}</div>
                 </Anime>
             </a>
         );
@@ -109,7 +112,7 @@ class Button extends Component {
                             </path>
                     </svg>
                     <div style={{"zIndex":"1000", ...this.props.textStyle}}
-                        className={this.props.normalTextClassName}>{this.buttonText}</div>
+                        className={this.normalTextClassName}>{this.buttonText}</div>
             </a>
         );
     }
